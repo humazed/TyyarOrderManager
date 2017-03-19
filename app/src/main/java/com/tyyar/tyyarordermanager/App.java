fcabafb21847ea5531dac7ffc885494e55c13c8e
humazed <humazed@gmail.com>
2017-03-19 02:40:02 +0200

    added LeakCanary

--- app/src/main/java/com/tyyar/tyyarordermanager/App.java
+++ app/src/main/java/com/tyyar/tyyarordermanager/App.java
@@ -3,6 +3,7 @@ package com.tyyar.tyyarordermanager;
 import android.app.Application;
 
 import com.blankj.utilcode.utils.Utils;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * User: YourPc
@@ -13,6 +14,13 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
         Utils.init(this);
     }
 }
